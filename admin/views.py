57ef7a7ae273bf2936cebbd680680a845b348085
ick <1031308775@qq.com>
2019-11-21 17:24:07 +0800

    1

--- admin/views.py
+++ admin/views.py
@@ -9,7 +9,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from dwebsocket.decorators import accept_websocket
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+import sys
+import logging
 
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+secret_id = 'AKID4wviFY9ckrkSuITusKXYP6f17xYsRnOj'      # 替换为用户的 secretId
+secret_key = 'HI4arreAQQCi293mGUSAduymNincEbke'      # 替换为用户的 secretKey
+region = 'ap-guangzhou'     # 替换为用户的 Region
+token = None                # 使用临时密钥需要传入 Token，默认为空，可不填
+scheme = 'http'            # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
+config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
+# 2. 获取客户端对象
+client = CosS3Client(config)
 
 # Create your views here.
 @accept_websocket
